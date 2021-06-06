a12e409a05b17a4e9e5aaa574464bab00f1c3b33
Varsha Raveendran <avr.varsha@gmail.com>
2021-06-06 21:44:28 +0200

    Add module names in __init__

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name='dlutils-eva6-group15',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1.1',
     description='Python package for computer vision',
     author='EVA6 Group 15',
     license='MIT',
