c72f0bb6a5ed4c84b5d8a4d22cace2add86fef72
abdksyed <abdksyed@gmail.com>
2021-06-13 16:47:15 +0530

    Albumentations Helper and Pipeline

--- src/dlutils/data/MNIST.py
+++ src/dlutils/data/MNIST.py
@@ -1,14 +1,23 @@
 import torch
 from torchvision import datasets, transforms
 
-class Data:
+class MNISTData:
     
     def __init__(self, mean=(0.1307,), std=(0.3081,)):
         self.mean = mean
         self.std = std
 
-    def build_train(self, transformations=None):
+    def build_data(self, transformations=None, *, train=True):
+        '''
+        Downloads MNIST Data from Torch Vision and performs trasnformations if given,
+        or uses pre determined mean and standard deviation of MNIST to convert to Tensor and Normalize.
 
+        Args:
+            trasnformations: List of Trasnformations Compose
+            train: True to get Train Data, False for Test Data.
+        Return:
+            Train/Test Dataset of 60,000/10,000 MNIST Images. 
+        '''
         if not transformations:
             transformations = transforms.Compose(
                                 [
@@ -16,6 +25,18 @@ class Data:
                                 transforms.Normalize((0.1307,), (0.3081,))
                                 ])
 
-        train_data = datasets.MNIST('../data', train=True, download=True, trasnform=transformations)
+        data = datasets.MNIST('../data', train=train, download=True, trasnform=transformations)
 
-        
+        return data
+
+    def build_loader(self, dataset, batch_size=64, **kwargs):
+        '''
+        Create the Train Loader for the given dataset, and batch_size
+        Args:
+            dataset: The MNIST Train Data Set
+            batch_size: The batch size.
+        '''
+        loader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=True, **kwargs)
+        return loader
+
+    
