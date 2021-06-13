b23085873c1a646dad8e701f04f23671880570f8
abdksyed <abdksyed@gmail.com>
2021-06-13 17:03:27 +0530

    Dataset Generator from Pytorch

--- src/dlutils/data/pytorch_data.py
+++ src/dlutils/data/pytorch_data.py
@@ -1,16 +1,20 @@
+from typing import Literal
+import operator
 import torch
 from torchvision import datasets, transforms
 
-class MNISTData:
+class GetData:
     
-    def __init__(self, mean=(0.1307,), std=(0.3081,)):
-        self.mean = mean
-        self.std = std
+    def __init__(self, name:Literal['MNIST', 'CIFAR10'], path='../data'):
+
+        self.path = path
+        name = name.upper()
+        self.gen_dataset = operator.attrgetter(name)(datasets)
 
     def build_data(self, transformations=None, *, train=True):
         '''
         Downloads MNIST Data from Torch Vision and performs trasnformations if given,
-        or uses pre determined mean and standard deviation of MNIST to convert to Tensor and Normalize.
+        or uses pre determined mean and standard deviation of MNIST to convert to Tensor.
 
         Args:
             trasnformations: List of Trasnformations Compose
@@ -18,18 +22,15 @@ class MNISTData:
         Return:
             Train/Test Dataset of 60,000/10,000 MNIST Images. 
         '''
-        if not transformations:
-            transformations = transforms.Compose(
-                                [
-                                transforms.ToTensor(),
-                                transforms.Normalize((0.1307,), (0.3081,))
-                                ])
 
-        data = datasets.MNIST('../data', train=train, download=True, trasnform=transformations)
+        if not transformations:
+            transformations = transforms.Compose([transforms.ToTensor()])
+
+        data = self.gen_dataset(self.path, train=train, download=True, trasnform=transformations)
 
         return data
 
-    def build_loader(self, dataset, batch_size=64, **kwargs):
+    def build_loader(self, dataset:datasets, batch_size=64, **kwargs):
         '''
         Create the Train Loader for the given dataset, and batch_size
         Args:
