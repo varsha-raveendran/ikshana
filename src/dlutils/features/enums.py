d10a2a3b92c21f3e25a7814e5d1544fe82eb5af0
syed <abdksyed@gmail.com>
2021-06-16 11:20:40 +0530

    Num Workers from 1 to 2

--- src/dlutils/features/enums.py
+++ src/dlutils/features/enums.py
@@ -3,4 +3,4 @@ import torch
 use_cuda = torch.cuda.is_available()
 DEVICE = torch.device("cuda" if use_cuda else "cpu")
 
-LOADER_kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
+LOADER_kwargs = {'num_workers': 2, 'pin_memory': True} if use_cuda else {}
